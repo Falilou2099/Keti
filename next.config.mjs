e3362e57309d24302ea8bafe4ae0fed1e978e110
dynanoir <tom.guittet0@gmail.com>
2026-01-29 11:37:42 +0100

    tom features : travaille avant fision avec la branche check-ia .1

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  turbopack: {
+    root: __dirname,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
